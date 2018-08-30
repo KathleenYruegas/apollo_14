dea653b9cedf6026b318cef7dc468e58c517eeed
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-30 08:49:55 -0600

    Adds average age

--- spec/features/user_can_see_all_astronauts_spec.rb
+++ spec/features/user_can_see_all_astronauts_spec.rb
@@ -1,18 +1,28 @@
 require "rails_helper"
 
 describe "When I visit /astronauts" do
-  it 'should show all astronauts and info' do
-    astro_1 = Astronaut.create!(name: "Neil Armstrong", age: 32, job: "Commander")
-    astro_2 = Astronaut.create!(name: "Buzz Aldrin", age: 30, job: "Pilot")
-    astro_3 = Astronaut.create!(name: "Bob Jones", age: 28, job: "Engineer")
+  before(:each) do
+    @astro_1 = Astronaut.create!(name: "Neil Armstrong", age: 32, job: "Commander")
+    @astro_2 = Astronaut.create!(name: "Buzz Aldrin", age: 30, job: "Pilot")
+    @astro_3 = Astronaut.create!(name: "Bob Jones", age: 28, job: "Engineer")
+  end
 
+  it 'should show all astronauts and info' do
     visit astronauts_path
 
-    expect(page).to have_content(astro_1.name)
-    expect(page).to have_content(astro_1.age)
-    expect(page).to have_content(astro_1.job)
-    expect(page).to have_content(astro_2.name)
-    expect(page).to have_content(astro_2.age)
-    expect(page).to have_content(astro_2.job)
+    expect(page).to have_content(@astro_1.name)
+    expect(page).to have_content(@astro_1.age)
+    expect(page).to have_content(@astro_1.job)
+    expect(page).to have_content(@astro_2.name)
+    expect(page).to have_content(@astro_2.age)
+    expect(page).to have_content(@astro_2.job)
+  end
+
+  it 'should show average age of all astronauts' do
+    visit astronauts_path
+
+    within("#ave-age") do
+      expect(page).to have_content(Astronaut.average_age)
+    end
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -6,4 +6,15 @@ describe Astronaut, type: :model do
     it { should validate_presence_of :age }
     it { should validate_presence_of :job }
   end
+
+  describe 'class methods' do
+    it '.average_age' do
+      astro_1 = Astronaut.create!(name: "Neil Armstrong", age: 32, job: "Commander")
+      astro_2 = Astronaut.create!(name: "Buzz Aldrin", age: 30, job: "Pilot")
+      astro_3 = Astronaut.create!(name: "Bob Jones", age: 28, job: "Engineer")
+
+      target = (astro_1.age + astro_2.age + astro_3.age) / 3.0
+      expect(Astronaut.average_age).to eq(target)
+    end
+  end
 end
